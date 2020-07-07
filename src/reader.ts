99b65d95301fa4a16ae323633860d5b0e8f729bf
XUTL <dev@xutl.es>
2020-07-07 15:24:36 +0100

    Added Reader Interface

---
+++ src/reader.ts
@@ -0,0 +1,62 @@
+import { promises as fs } from 'fs';
+import { gunzip } from 'zlib';
+import { PkgIndex } from './info';
+import { readIndex } from './package';
+
+export class Reader implements AsyncIterable<Buffer> {
+	readonly #filename: string;
+	readonly #index: Promise<PkgIndex>;
+	constructor(filename: string) {
+		this.#filename = filename;
+		this.#index = getIndex(filename);
+	}
+	get entries() {
+		return this.#index.then((index) => Object.keys(index));
+	}
+	async buffer(name: string): Promise<Buffer> {
+		name = new URL(name, `pkg://${(await this.#index).name}/`).pathname;
+		const index = await this.#index;
+		if (!(name in index.entries)) throw new Error(`not found: ${name}`);
+		const { start, length } = index.entries[name];
+		const fd = await fs.open(this.#filename, 'r');
+		const result = Buffer.alloc(length);
+		const { bytesRead } = await fd.read(result, 0, length, start);
+		if (bytesRead !== length) throw new Error(`only read ${bytesRead} bytes rather than ${length}`);
+		return unzip(result);
+	}
+	async string(name: string) {
+		const buffer = await this.buffer(name);
+		return buffer.toString('utf-8');
+	}
+	async json(name: string) {
+		const string = await this.string(name);
+		return JSON.parse(string);
+	}
+	[Symbol.asyncIterator]() {
+		let items: string[];
+		return {
+			next: async (): Promise<IteratorResult<Buffer>> => {
+				items = items || (await this.entries);
+				const item = items.shift();
+				if (!item) return { done: true, value: null };
+				const value = await this.buffer(item);
+				return { done: false, value };
+			},
+		};
+	}
+}
+
+async function getIndex(filename: string) {
+	const fd = await fs.open(filename, 'r');
+	const { index } = await readIndex(fd);
+	await fd.close();
+	return index;
+}
+async function unzip(buffer: Buffer): Promise<Buffer> {
+	return new Promise((resolve, reject) => {
+		gunzip(buffer, (error: Error | null, result: Buffer) => {
+			if (error) return reject(error);
+			resolve(result);
+		});
+	});
+}
